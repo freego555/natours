09c3115a52fb76bc57b2605d8da9c37361c709ba
Ihor Rohovets <freego555@gmail.com>
2021-10-07 10:58:37 +0300

    Lesson 94. Making the API Better: Advanced Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,13 +1,18 @@
-const Tour = require('./../models/tourModel');
+const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // 2) Advanced filtering with MongoDB operators gte, gt, lte, lt
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryStr));
 
     // const query = Tour.find({
     //   duration: 5,
