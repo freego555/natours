ce755ec1397635df4ccf56eb5ff005db5094384d
Ihor Rohovets <freego555@gmail.com>
2021-10-21 14:33:02 +0300

    Lesson 121. Errors Outside Express: Unhandled Rejections

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ mongoose
   .then(() => console.log('DB connect is successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
