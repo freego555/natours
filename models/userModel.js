f057bd61a43dddc1d06eb5f46a434ce6f5de3287
Ihor Rohovets <freego555@gmail.com>
2021-10-22 21:41:37 +0300

    Lesson 126. Managing Passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -34,7 +35,26 @@ const userSchema = new mongoose.Schema({
       50,
       'A user password must have less than or equal 50 characters',
     ],
+    validate: {
+      // This only works on CREATE and SAVE!!!
+      validator: function (el) {
+        return this.password === el;
+      },
+      message: 'Passwords are not the same.',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
