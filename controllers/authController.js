b7482ca1e0a5da70467542955fb73f389bc4e5bf
Ihor Rohovets <freego555@gmail.com>
2021-10-27 07:34:53 +0300

    Lesson 134. Password Reset Functionality: Reset Token

--- controllers/authController.js
+++ controllers/authController.js
@@ -108,3 +108,21 @@ exports.restrictTo =
 
     next();
   };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(
+      new AppError('There is now user with specified email address.', 404)
+    );
+  }
+
+  // 2) Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save();
+
+  // 3) Send token to user's email
+});
+
+exports.resetPassword = (req, res, next) => {};
