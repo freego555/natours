7e59573e15f373432d33d43bde934cb2111cf671
Ihor Rohovets <freego555@gmail.com>
2021-11-02 17:57:08 +0200

    S11. Lesson 156. Virtual Populate: Tours and Reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -38,10 +38,12 @@ const reviewSchema = new mongoose.Schema(
 
 // QUERY MIDDLEWARE
 reviewSchema.pre(/^find/, function (next) {
-  this.populate({
-    path: 'tour',
-    select: 'name',
-  }).populate({
+  this
+    // .populate({
+    //     path: 'tour',
+    //     select: 'name',
+    //   })
+    .populate({
     path: 'user',
     select: 'name photo',
   });
