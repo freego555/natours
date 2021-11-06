f5d76d414d1b64817103e97352d8f05139618acd
Ihor Rohovets <freego555@gmail.com>
2021-11-06 17:59:55 +0200

    S11. Lesson 171. Geospatial Aggregation: Calculating Distances

--- models/tourModel.js
+++ models/tourModel.js
@@ -172,14 +172,14 @@ tourSchema.post(/^find/, function (docs, next) {
 });
 
 // AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({
-    $match: { secretTour: { $ne: true } },
-  });
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({
+//     $match: { secretTour: { $ne: true } },
+//   });
 
-  console.log(this);
-  next();
-});
+//   console.log(this);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
